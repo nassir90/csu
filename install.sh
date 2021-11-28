2c7adcce9923651fa821e7cecd84d52622239bce
Naza <nozolo90@gmail.com>
2021-11-28 20:07:31 +0000

    Hmm

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cp ./csu ~/bin/csu
+[ ! -d ~/bin ] && echo "~/bin doesn't exist. Creating..." && mkdir ~/bin
+! grep '\$HOME/bin\|~/bin' ~/.profile > /dev/null && echo "~/bin is not in path. Adding entry to ~/.profile" && echo 'export PATH="$PATH:$HOME/bin"' >> ~/.profile
+cp csu ~/bin/
