8e83794a076dee6973349032f53c3da4620c7958
Naza <nozolo90@gmail.com>
2022-01-21 09:27:34 +0000

    No longer editing the users .profile file. That was silly

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 [ ! -d ~/bin ] && echo "~/bin doesn't exist. Creating..." && mkdir ~/bin
-! grep '\$HOME/bin\|~/bin' ~/.profile > /dev/null && echo "~/bin is not in path. Adding entry to ~/.profile" && echo 'export PATH="$PATH:$HOME/bin"' >> ~/.profile
+echo -n $PATH | grep -q "$HOME/bin" || echo "~/bin is not in your PATH! Installation will have no effect."
 cp csu ~/bin/
